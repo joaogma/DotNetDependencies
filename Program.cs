ad7e3450f807ecae939daf2e5d30fe081a05fb7c
João Amendola <mx.joaoo@gmail.com>
2021-10-11 21:46:41 -0300

    Learning to use github and vscode: my first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Humanizer;
+
+namespace DotNetDependencies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
